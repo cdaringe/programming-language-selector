f008f28823720e9ecb991f8a8b0e9b258e68c33a
cdaringe <cdaringe@gmail.com>
2023-08-13 18:58:02 -0700

    chore: add deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
 
 module.exports = nextConfig;
