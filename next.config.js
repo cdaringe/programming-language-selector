674542e560852c964e05d2f8c4bc989a506598e3
cdaringe <cdaringe@gmail.com>
2023-08-13 19:10:02 -0700

    fix: export mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
